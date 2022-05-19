05f7a5284e66e8161780688415d2b16d6960cdc6
Gregory Bell <trihard.studios@outlook.com>
2022-05-19 14:39:18 -0600

    Started outlining loaders

---
+++ loaders.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+'''
+This function validates that a CSV file with the name '_filename' exists 
+If it does, it loads it in to a Pandas dataframe to be returned 
+'''
+
+
+def loadCSV(_filename):
+    pass
+
+
+'''
+This function loads the select grade scope CSV file
+'''
+
+
+def loadGradescope(_filename):
+    pass
+
+
+def loadCanvas(_filename):
+    pass
