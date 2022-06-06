c2370dd65225c7903a8d48e41aa269cd2f02130e
Gregory Bell <trihard.studios@outlook.com>
2022-06-05 20:50:14 -0600

    Cleaned up comments to follow python standards

--- config.py
+++ config.py
@@ -2,43 +2,39 @@ import json
 import os
 from Canvas import Canvas
 
-'''
-This function reads the config files from the './config/' directory.
-It filters out any non files and any files that don't end in .json
-Returns the number of found files and a list of the files
-'''
-
 
 def locateConfigFiles():
+    """
+    This function reads the config files from the './config/' directory.
+    It filters out any non files and any files that don't end in .json
+    Returns the number of found files and a list of the files
+    """
+
     configFileName = [f"./config/{f}" for f in os.listdir("./config/") if
                       os.path.isfile(f"./config/{f}") and ".json" in f]
     return len(configFileName), configFileName
 
 
-'''
-This function reads the config from the disk and converts it to a python dictionary.
-PARAMS:
-    _configFileName - The file name to load
-'''
-
-
 def readConfig(_configFileName):
+    """
+    This function reads the config from the disk and converts it to a python dictionary.
+    PARAMS:
+        _configFileName - The file name to load
+    """
     with open(_configFileName, "r") as jsonDataFile:
         configFile = json.load(jsonDataFile)
 
     return configFile
 
 
-'''
-Creates a new config file. 
-Config file must contain an API key,
-the course ID, the user ID, and the canvas endpoint.
-It also must contain the assignment list.
-This function walks the user through the creation, and builds a new config file to be saved and loaded.
-'''
-
-
 def createNewConfig():
+    """
+    Creates a new config file.
+    Config file must contain an API key,
+    the course ID, the user ID, and the canvas endpoint.
+    It also must contain the assignment list.
+    This function walks the user through the creation, and builds a new config file to be saved and loaded.
+    """
     apiKey = str(input("Enter your Canvas API key: "))
     userId = str(input("Enter your Canvas user id: "))
     endpoint = str(input("Enter your Canvas endpoint: "))
@@ -104,13 +100,12 @@ def createNewConfig():
     return output
 
 
-'''
-This function gets the list of available config files and prompts the user to create a new file if none exist or choose 
-file to open from a list. If only one file exists then it will load that file by default.
-'''
-
-
 def loadConfig():
+    """
+    This function gets the list of available config files and prompts the user to create a new file if none exist
+    or choose file to open from a list. If only one file exists then it will load that file by default.
+    Returns the loaded config file. (unless one isnt created, in which case it exits the program)
+    """
     configFileCount, configFiles = locateConfigFiles()
     if configFileCount == 1:
         return readConfig(configFiles[0])
