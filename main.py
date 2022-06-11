40855cd464123b6c2f2ea2413f0b848959acf86f
Gregory Bell <trihard.studios@outlook.com>
2022-06-11 15:10:02 -0600

    Removed useless comments; updated some sample files

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ hw2Grades = None
 specialCases = None
 assignmentsToPost = None
 
+
 def run():
     global hw2Grades, specialCases, assignmentsToPost
-    hw2Grades = ld.loadGradescope("sample_files/HW13_Vocab_scores.csv")
-    specialCases = ld.loadSpecialCases("sample_files/special_cases.csv", ["HW13"])
+    hw2Grades = ld.loadGradescope("sample_files/HW2_Scavenger_Hunt_scores.csv")
+    specialCases = ld.loadSpecialCases("sample_files/special_cases.csv", ["HW2"])
     hw2Grades = grade.scoreMissingAssignments(hw2Grades)
     hw2Grades = grade.scaleScores(hw2Grades, .1, maxScore=7)
 
-    hw2Grades, specialCases = grade.calculateLatePenalty(hw2Grades, specialCases)
+    hw2Grades, specialCases = grade.calculateLatePenalty(hw2Grades, specialCases, "HW2")
     configFile = config.loadConfig()
 
     canvas = Canvas()
