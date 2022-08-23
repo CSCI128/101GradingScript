d471c6cfb67bf917a3361361c7e6f6581da5d4da
Gregory Bell <trihard.studios@outlook.com>
2022-08-22 21:22:07 -0600

    Changes to prevent build from breaking when run with signle file exe

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
+import sys
 from Canvas import Canvas
 import config
 from UI.ui import mainMenu
-
 
 def main():
     # TODO May want to rework this config loading!
@@ -20,4 +21,11 @@ def main():
 
 
 if __name__ == "__main__":
+    # when the program is compiled, it will execute in a tmp folder, which is unhelpful when reading data in
+    #  so to work around that, we are checking to see if we are running in that mode, then updating the current working
+    #  directory to be where the app is downloaded.
+
+    if getattr(sys, 'frozen', False):
+        # print(os.path.dirname(sys.executable))
+        os.chdir(os.path.dirname(sys.executable))
     main()
