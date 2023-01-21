4ac1468f1f7dfe4c1f4c2754e3c098779f009df8
Gregory Bell <gjbell@hackbook-pro.local>
2023-01-21 15:25:46 -0700

    Added list of names for factories; started work on factories

---
+++ test/Factories.py
@@ -0,0 +1,13 @@
+
+class Factories:
+    def generateStudentRoaster(cls, numberOfStudents=10, studentEmailFormat="<username>@realschool.edu") -> dict:
+        """
+        :Description:
+
+        This function generates a sample roaster for a canvas class.
+
+        """
+        pass
+
+    def generateStudentGradesForAssignment(cls):
+        pass
---
+++ test/TestingFramework.py
@@ -0,0 +1,8 @@
+import unittest
+import unittest.mock
+
+
+def mock_requestsGet(*args, **kwargs):
+    pass
+
+
